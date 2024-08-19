5c465c223399f8c400e8c0fad0c60eaedc680f12
Guilherme Gomes do Nascimento <98121832+itsGuigo@users.noreply.github.com>
2024-08-19 09:39:31 -0300

    Add files via upload

---
+++ ViaCepModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiViaCep.Classes
+{
+    public class ViaCepModel
+    {
+        public string? Erro {  get; set; }
+
+        [JsonProperty("cep")]
+        public string CEP {  get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonProperty("complemento")]
+        public string? Completemento { get; set; }
+
+        [JsonProperty("unidade")]
+        public string? Unidade { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+
+        [JsonProperty("uf")]
+        public string UF { get; set; }
+
+        [JsonProperty("ibge")]
+        public string IBGE { get; set; }
+
+        [JsonProperty("gia")]
+        public string GIA { get; set; }
+
+        [JsonProperty("ddd")]
+        public string DDD { get; set; }
+
+        [JsonProperty("siafi")]
+        public string SIAFI { get; set; }
+
+        public void AddError(string erro) => Erro = erro;
+    }
+}
